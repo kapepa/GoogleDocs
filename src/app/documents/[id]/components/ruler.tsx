4450b0d94d92b53ed58d12ba18e162a32d88dfd2
kapepa <kapepayx@gmail.com>
2025-01-29 17:14:19 +0200

    component, tools

--- src/app/documents/[id]/components/ruler.tsx
+++ src/app/documents/[id]/components/ruler.tsx
@@ -3,8 +3,6 @@
 import { FC, MouseEvent, useRef, useState } from "react";
 import { Marker } from "./marker";
 
-
-
 const markers = Array.from({ length: 83 }, (_, i) => i);
 
 const Ruler: FC = () => {
@@ -68,11 +66,11 @@ const Ruler: FC = () => {
       onMouseMove={handleMouseMove}
       onMouseUp={handleMouseUp}
       onMouseLeave={handleMouseUp}
-      className="h-6 border-b border-gray-300 flex items-end relative select-none print:hidden"
+      className="w-[816px] mx-auto h-6 border-b border-gray-300 flex items-end relative select-none print:hidden"
     >
       <div
         id="ruler-container"
-        className="max-w-[816px] mx-auto w-full h-full relative"
+        className="w-full h-full relative"
       >
         <Marker
           position={leftMargin}
--- src/app/documents/[id]/page.tsx
+++ src/app/documents/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from "next";
 import { Editor } from "./components/editor";
 import { Toolbar } from "./components/toolbar";
+import { Navbar } from "./components/navbar";
 
 interface DocumentsIdPageProps {
   params: Promise<{ id: string }>
@@ -13,8 +14,17 @@ const DocumentsIdPage: NextPage<DocumentsIdPageProps> = async (props) => {
     <div
       className="min-h-screen bg-[#FAFBFD]"
     >
-      <Toolbar />
-      <Editor />
+      <div
+        className="flex flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 z-10 bg-[#FAFBFD] print:hidden h-[112px]"
+      >
+        <Navbar />
+        <Toolbar />
+      </div>
+      <div
+        className="pt-[114px] print:pt-0"
+      >
+        <Editor />
+      </div>
     </div>
   )
 }
